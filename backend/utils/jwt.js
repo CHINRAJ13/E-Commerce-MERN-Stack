5ed01792275659466f2eda117245a6ce42f8c227
CHINRAJ13 <142643664+CHINRAJ13@users.noreply.github.com>
2025-03-31 13:32:37 +0530

    Update jwt.js

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -7,6 +7,8 @@ const sendToken = ( user, statusCode, res ) => {
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE_TIME *24 *60 *60 *1000),
         httpOnly: true,
+        secure: true,
+        sameSite: "Strict"
     }
 
     res.status(statusCode)
